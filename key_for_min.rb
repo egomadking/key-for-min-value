17df0b40555930627f6c5326797ed852e1aa0772
Roy Mosby <ego.madking@gmail.com>
2020-12-11 14:45:47 -0500

    commit tree ab1578367a653847f364c4d48097facb82913847

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  holder = ["", 0]
+  if name_hash == {} || name_hash == nil
+    return nil
+  else
+    name_hash.map do |name, num|
+      if holder[0] == "" && holder[1] == 0
+        holder = [name, num]
+      elsif num < holder[1]
+        holder = [name, num]
+      end
+    end
+  end
+  holder[0]
 end
